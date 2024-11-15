02d384202f07462399930aaffa641e624b0c831c
Italo Git <sulamita.italo12@gmail.com>
2024-11-15 00:23:38 -0300

    conexão com o banco de dados do railway

--- PHP/cadastro.php
+++ PHP/cadastro.php
@@ -1,17 +1,43 @@
 <?php
 
+// Recebendo os dados do formulário
 $nomeSindico = $_POST['nomeSindico'];
+$email = $_POST['email'];
 $nomeCondominio = $_POST['nomeCondominio'];
 $nomeRua = $_POST['nomeRua'];
 $numeroCondominio = $_POST['numeroCondominio'];
 $estadoCondominio = $_POST['estadoCondominio'];
-$salas = json_decode($_POST['salasArmazenado'], true);
+$salas = json_decode($_POST['salasArmazenado'], true); 
 
-$connect = mysqli_connect('jdbc:mysql://junction.proxy.rlwy.net:10676/railway', 'root', 'VzXXsFUcaDRjnHrRbgFycSQzayDyKASl', 'railway');
+$connect = mysqli_connect('junction.proxy.rlwy.net', 'root', 'VzXXsFUcaDRjnHrRbgFycSQzayDyKASl', 'railway', 10676);
 
-if($connect) {
-    echo "Banco de Dados Criado";
+if ($connect) {
+    $query = "INSERT INTO usuarios (nomeSindico, email, nomeCondominio, nomeRua, numeroCondominio, estadoCondominio, salas) 
+              VALUES (?, ?, ?, ?, ?, ?, ?)";
+    
+    $stmt = mysqli_prepare($connect, $query);
+    
+
+    if ($stmt) {
+        $salasJson = json_encode($salas);
+        
+        mysqli_stmt_bind_param($stmt, 'ssssss', $nomeSindico, $email, $nomeCondominio, $nomeRua, $numeroCondominio, $estadoCondominio, $salasJson);
+        
+        if (mysqli_stmt_execute($stmt)) {
+            echo "Dados inseridos com sucesso!";
+        } else {
+            echo "Erro ao executar a query: " . mysqli_stmt_error($stmt);
+        }
+        
+        mysqli_stmt_close($stmt);
+    } else {
+        echo "Erro ao preparar a query: " . mysqli_error($connect);
+    }
 } else {
-    echo "Erro";
+    echo "Erro ao conectar com o banco de dados: " . mysqli_connect_error();
 }
+
+// Fechando a conexão
+mysqli_close($connect);
+
 ?>
