2b5c4a651d6c07c3b131dfedf5a9eab726d3ead1
Marceloalen07 <167093974+Marceloalen07@users.noreply.github.com>
2024-11-22 16:30:27 -0300

    back-end pt3

--- backend/login.php
+++ backend/login.php
@@ -1,26 +1,26 @@
 <?php 
-session_start(); //Inicia a sessão
-require "./conexaoBD.php"; //Conexão com o banco de dados
+session_start(); // Inicia a sessão
+require "./conexaoBD.php"; // Conexão com o banco de dados
 
-$email = $_POST['email']; //Email
-$senha = $_POST['password']; //Senha
+$email = $_POST['email']; // Email
+$senha = $_POST['password']; // Senha
 
-$query_dados = "SELECT email , senha From usuarios Where email = ?"; //Comando SQL
+$query_dados = "SELECT email , senha From usuarios Where email = ?"; // Comando SQL
 
-$stmt = mysqli_prepare( $connect , $query_dados); //Cria a conexeção e prepara o aplicamento dos dados
+$stmt = mysqli_prepare( $connect , $query_dados); // Cria a conexeção e prepara o aplicamento dos dados
 
 if($stmt){
     mysqli_stmt_bind_param($stmt, 's', $email); // Passa os parametros
     mysqli_stmt_execute($stmt); // executa o comando sql
 
-    $result = mysqli_stmt_get_result($stmt); //mostra o resultado
-    if($row = mysqli_fetch_assoc($result)){ //Pega o resultado por colunas
+    $result = mysqli_stmt_get_result($stmt); // mostra o resultado
+    if($row = mysqli_fetch_assoc($result)){ // Pega o resultado por colunas
         $emailBD = $row['email']; // Coluna de Email
         $senhaBD = $row['senha']; // Coluna de Senha
         if($emailBD == $email && $senhaBD == $senha) {
             $_SESSION['email'] = $email; // Adicionando valor a sessão
             header("location", "../Paginas/calendar.html")
-            echo "<script>
+           echo "<script>
                 window.location.href = '../Paginas/calendar.html';
             </script>"; //alert do js
         } else {
@@ -30,5 +30,4 @@ if($stmt){
             </script>"; //alert do js
         }
     }
-
 }
