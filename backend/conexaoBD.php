1a6a907f4d8371f9aecc0303b281cc316906ec70
Marceloalen07 <167093974+Marceloalen07@users.noreply.github.com>
2024-11-21 21:06:25 -0300

    codigo backend

---
+++ backend/conexaoBD.php
@@ -0,0 +1,9 @@
+<?php 
+$connect = mysqli_connect('junction.proxy.rlwy.net', 'root', 'VzXXsFUcaDRjnHrRbgFycSQzayDyKASl', 'railway', 10676); // Conexão com o Railway onde está hospedado o banco de dados!
+
+
+
+
+
+
+
---
+++ backend/lista_salas.php
@@ -0,0 +1,6 @@
+<?php 
+//inicia a sessão para acessar váriáveis de sessão
+session_start()
+
+//inclui o arquivo que conecta o banco de dados
+require './conexaoBD.php';
