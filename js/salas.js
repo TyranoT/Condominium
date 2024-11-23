bc6828d27f44507c6aa551276b07a9d07cef7795
Italo Git <sulamita.italo12@gmail.com>
2024-11-22 21:34:58 -0300

    Update salas.js

--- js/salas.js
+++ js/salas.js
@@ -1,18 +1,18 @@
-const salas = []
+const salas = [] // Array onde será colocado os dados!
  function add(event) {
-    event.preventDefault()
-    const lugar = document.getElementById(`sala`).value
-    salas.push(lugar)
-    document.getElementById(`escondidinho`).value = JSON.stringify(salas)
-    update()
+    event.preventDefault() // Cancelar o onClick padrão de submit
+    const lugar = document.getElementById(`sala`).value // Input do valor de salas
+    salas.push(lugar) // Função para colocar os valores dentro do Array
+    document.getElementById(`escondidinho`).value = JSON.stringify(salas) // Pega o input invisivel
+    update() // Função de Atualizar
  }
 
  function update() {
-    const lista = document.getElementById(`lista`)
-    lista.innerHTML = ``
+    const lista = document.getElementById(`lista`) // Pega o span list
+    lista.innerHTML = `` // Zera
     salas.forEach((a) => {
-        const itens = document.createElement(`li`)
-        itens.textContent = a
-        lista.appendChild(itens)
+        const itens = document.createElement(`li`) // Cria um elemento <li>
+        itens.textContent = a // Valor
+        lista.appendChild(itens) // Coloca dentro de lista os elementos!
     })
  }
